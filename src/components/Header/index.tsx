b6a549c8d66fb6893a1bc4ca82e3071acb94f360
Jheovanny Campos <jheovanny06@hotmail.com>
2021-09-16 11:01:52 -0300

    Projeto Criado

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+//import { useState } from 'react'
+import logoImg from '../../assets/logo.svg'
+//import Modal from 'react-modal'
+import { Container, Content } from './styles'
+
+interface HeaderProps {
+    onOpenNewTransactionModal:() => void;
+}
+
+export function Header( {onOpenNewTransactionModal} : HeaderProps) {
+
+    return(
+        <Container>
+            <Content>
+             <img src={ logoImg } alt="dt money" />
+            <button type="button" onClick={onOpenNewTransactionModal}>
+                Nova transação
+            </button>
+            </Content>
+        </Container>
+    )
+}
